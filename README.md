d90a0b5841b24dd6e77aa6625e906d7908474376
melihs <melihsahin24@gmail.com>
2019-08-15 16:16:32 +0300

    yargs package added and completed app

--- README.md
+++ README.md
@@ -1,16 +1,37 @@
-# password-manager-app
+# Account-manager-app
 
-> fundemental node.js application 
+> fundemental console-based node.js application
+![console-based node.js application](nodejs-app.gif)
 
 ## Dependencies in the Project
 - node-persist https://github.com/simonlast/node-persist
+- yargs https://github.com/yargs/yargs
 
 ## Build Setup
 
 ``` bash
 # install dependencies
 npm install
+```
 
-# start project
-npm start
+# Usage
+Commands 
+
+#####Create new account
+```
+ node app.js create -n <account-name> -u <user-name> -p <password>
+```
+#####Get account
+```
+ node app.js get -n <account-name>
+```
+
+#####Aliases
+```
+ create : "Cretae new account" command
+ get    : "Get Account" command
+ -n : "account name"
+ -u : "username"
+ -p : "password"
+```
 
--- app.js
+++ app.js
@@ -1,6 +1,67 @@
-let storage = require("node-persist");
-
+const storage = require("node-persist");
 storage.initSync();
+
+const argv = require("yargs")
+    .command('create','cretae new account',function(yargs){
+        yargs.options({
+            name : {
+                demand : true,
+                description : 'Account name!',
+                alias : 'n',
+                type : 'string'
+            },
+            username : {
+                demand : true,
+                description : 'Account owner name!',
+                alias : 'u',
+                type : 'string'
+            },
+            password : {
+                demand : true,
+                description : 'Account password',
+                alias : 'p',
+                type : 'string'
+            }
+        }).help('help');
+    })
+    .command('get','Get Account',function(yargs){
+        yargs.options({
+            name : {
+                demand :true,
+                description : 'Account name',
+                alias : 'n',
+                type : 'string'
+
+            }
+        }).help('help');
+    })
+    .argv;
+let command = argv._[0];
+
+if( command === 'create'
+    && typeof argv.name !== 'undefined' && argv.name.length > 0
+    && typeof argv.username !== 'undefined' && argv.username.length > 0
+    && typeof argv.password !== 'undefined' && argv.password.length > 0 ) {
+
+   let createdAccount = createAccount({
+        name : argv.name,
+        username : argv.username,
+        password : argv.password,
+    });
+
+   console.log('Account created successfully...');
+
+}else if(command === 'get' && typeof argv.name !== 'undefined' && argv.name.length > 0) {
+    let account = getAccount(argv.name);
+
+    if(typeof account !== 'undefined') {
+        console.log(account);
+    } else {
+        console.log('Opps! not found');
+    }
+} else {
+    console.log('Please enter current command!');
+}
 
 
 /**
@@ -32,21 +93,10 @@ function getAccount(accountName)
     let matchedAccount;
 
     accounts.forEach(function(account) {
-            if(account.name == accountName) {
-                matchedAccount = account;
-            }
+        if(account.name == accountName) {
+            matchedAccount = account;
+        }
     });
 
     return matchedAccount;
 }
-
-
-// createAccount({
-//     name: "twitter",
-//     username: "test@gmail.com",
-//     password: "123!sfsf",
-// });
-
-let twitterAccount = getAccount("twitter");
-
-console.log(twitterAccount);
